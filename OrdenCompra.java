e1ceb4660a58bd9a06058ab5240dc1a4a8418454
David-1221-git <merinodominguezdavid@gmail.com>
2026-03-26 09:06:01 -0600

    Clase ordenVenta y Compra

--- OrdenCompra.java
+++ OrdenCompra.java
@@ -1,3 +1,29 @@
-public class OrdenCompra {
+public class OrdenCompra extends Orden {
+    private double montoInversion;
+    public OrdenCompra(Empresa empresa, double precioObjetivo, double montoInversion) {
+        super(empresa, precioObjetivo);
+        this.montoInversion = montoInversion;
+    }
+    @Override
+    public String getTipo() {
+        return "COMPRA";
+    }
+    @Override
+    public boolean ejecutar(double precioActual) {
+        if (precioActual <= getPrecioObjetivo()) {
+            System.out.println("Orden de COMPRA ejecutada para.- " + getEmpresa().getNombre() + 
+                               " a un precio de " + precioActual + " por acción.");
+            return true;
+        } else {
+            System.out.println("Orden de COMPRA no ejecutada para.- " + getEmpresa().getNombre() + 
+                               ". Precio actual (" + precioActual + ") es mayor que el precio objetivo (" + getPrecioObjetivo() + ").");
+            return false;
+        }
+    }
+      @Override
+    public String toString() {
+        return String.format("Orden COMPRA: %s | Precio Objetivo: %.2f | " +
+                           "Monto: %.2f", empresa.getNombre(), precioObjetivo, montoInversion);
+    }
     
 }
--- OrdenVenta.java
+++ OrdenVenta.java
@@ -1,3 +1,32 @@
-public class OrdenVenta {
-    
+public class OrdenVenta extends Orden {
+    private int cantidadVender;
+
+    public OrdenVenta(Empresa empresa, double precioObjetivo, int cantidadVender) {
+        super(empresa, precioObjetivo);
+        this.cantidadVender = cantidadVender;
+    }
+
+    @Override
+    public String getTipo() {
+        return "VENTA";
+    }
+
+    @Override
+    public boolean ejecutar(double precioMercado) {
+        if (precioMercado >= precioObjetivo) {
+            System.out.println("Orden de VENTA ejecutada.- " + cantidadVender + 
+                             " acciones de " + empresa.getNombre() + 
+                             " a " + precioMercado);
+            return true;
+        }
+            System.out.println("Orden de VENTA no ejecutada para.- " + empresa.getNombre() + 
+                                ". Precio actual (" + precioMercado + ") es menor que el precio objetivo (" + precioObjetivo + ").");
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Orden VENTA: %s | Precio Objetivo: %.2f | " +
+                           "Cantidad: %d", empresa.getNombre(), precioObjetivo, cantidadVender);
+    }
 }
